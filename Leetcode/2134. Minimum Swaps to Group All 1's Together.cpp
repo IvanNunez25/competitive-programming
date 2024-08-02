c032aac2ece8231de80ed4fed0d28c8ccb9780d9
Ivanovicx Nuñez <ivan25606@gmail.com>
2024-08-01 23:03:51 -0600

    sliding window

---
+++ Leetcode/2134. Minimum Swaps to Group All 1's Together.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minSwaps(vector<int>& nums) {
+    // Initialize minimum swaps to a large value
+    int minimumSwaps = INT_MAX;
+
+    // Calculate the total number of 1s in the array
+    int totalOnes = accumulate(nums.begin(), nums.end(), 0);
+
+    // Initialize the count of 1s in the current window
+    int onesCount = nums[0];
+    int end = 0;
+
+    // Slide the window across the array
+    for (int start = 0; start < nums.size(); ++start) {
+        // Adjust onesCount by removing the element that is sliding out of
+        // the window
+        if (start != 0) {
+            onesCount -= nums[start - 1];
+        }
+
+        // Expand the window to the right until it reaches the desired size
+        while (end - start + 1 < totalOnes) {
+            end++;
+            onesCount += nums[end % nums.size()];
+        }
+
+        // Update the minimum number of swaps needed
+        minimumSwaps = min(minimumSwaps, totalOnes - onesCount);
+    }
+
+    return minimumSwaps;
+}
+
+int main()
+{
+    vector < int > nums = {0,1,0,1,1,0,0};
+    cout << minSwaps( nums );
+
+    return 0;
+}
